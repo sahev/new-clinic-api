3ceb065d61564f7d32712c5e10a4c10019b8f740
sahev <s4mus.sh@gmail.com>
2023-05-17 13:46:31 -0300

    ds

--- deployprd.sh
+++ deployprd.sh
@@ -5,7 +5,17 @@ echo "built docker images and proceeding to delete existing container"
 result=$( docker ps -q -f name=eclinic )
 if [[ $? -eq 0 ]]; then
 echo "Removing container"
-docker container rm -f eclinic
+for PORT in 3000 3001 3002
+do
+  echo "Looking for a container on port: ${PORT}"
+  ID=$(\
+    docker container ls --format="{{.ID}}\t{{.Ports}}" |\
+    grep ${PORT} |\
+    awk '{print $1}')
+  echo "Found Container ID: ${ID}"
+  echo "Stopping and removing it"
+  docker container stop ${ID} && docker container rm ${ID}
+done
 docker rmi -f eclinic
 echo "Deleted the existing docker container"
 else
