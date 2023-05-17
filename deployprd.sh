91687873d8a859a5560011314fc13ea24ddd909f
sahev <s4mus.sh@gmail.com>
2023-05-17 13:33:10 -0300

    add sh

---
+++ deployprd.sh
@@ -0,0 +1,24 @@
+git reset --hard origin/master
+git pull
+result=$(docker images -q eclinic )
+if [[ -n "$result" ]]; then
+echo "removing image"
+docker rmi -f eclinic
+else
+echo "building the docker image"
+docker build -t eclinic .
+fi
+
+echo "built docker images and proceeding to delete existing container"
+result=$( docker ps -q -f name=eclinic )
+if [[ $? -eq 0 ]]; then
+echo "Removing container"
+sudo docker container rm -f eclinic
+echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+echo "Deploying the updated container"
+sudo docker run -itd -p 3000:3000 -d --name eclinic
+echo "Deploying the container"
